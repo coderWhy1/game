56594a9dd0d4b3a0c296f07da10393a02caf400e
小安 <3067597821@qq.com>
2026-07-14 01:00:10 +0800

    贪吃蛇修改

--- 贪吃蛇/js/main.js
+++ 贪吃蛇/js/main.js
@@ -6,12 +6,66 @@ var start = document.querySelector('.start')
 var controlMask = document.querySelector('.control-mask')
 var control = document.querySelector('.control')
 var audio = document.querySelector('.audio')
+var lawn = document.querySelector('.lawn')
+var wallTop = document.querySelector('.wall-top')
+var wallBottom = document.querySelector('.wall-bottom')
+var wallLeft = document.querySelector('.wall-left')
+var wallRight = document.querySelector('.wall-right')
 var speed = 160
+var cellSize = 16
+var maxCols = 24
+var maxRows = 40
+
+function resizeGame() {
+  var titleHeight = 0
+  var controlHeight = 180
+  var screenWidth = window.innerWidth
+  var screenHeight = window.innerHeight
+  var availableWidth = screenWidth
+  var availableHeight = screenHeight - titleHeight - controlHeight
+
+  var wallThickness = 22
+  var wallOverlap = 10
+  var wallExtraX = wallOverlap * 2 + wallThickness * 2
+  var wallExtraY = wallThickness * 2 + wallOverlap * 2
+
+  var innerWidth = availableWidth - wallExtraX
+  var innerHeight = availableHeight - wallExtraY
+
+  var cols = Math.floor(innerWidth / cellSize)
+  var rows = Math.floor(innerHeight / cellSize)
+
+  maxCols = Math.max(10, Math.min(cols, 80))
+  maxRows = Math.max(12, Math.min(rows, 100))
+
+  var lawnWidth = maxCols * cellSize
+  var lawnHeight = maxRows * cellSize
+
+  lawn.style.width = lawnWidth + 'px'
+  lawn.style.height = lawnHeight + 'px'
+  lawn.style.transform = 'none'
+
+  var topBottomWidth = lawnWidth + wallOverlap * 2
+  var leftRightWidth = lawnHeight + wallOverlap * 2
+
+  wallTop.style.width = topBottomWidth + 'px'
+  wallBottom.style.width = topBottomWidth + 'px'
+
+  wallLeft.style.width = leftRightWidth + 'px'
+  wallLeft.style.left = -(leftRightWidth + wallThickness) + 'px'
+
+  wallRight.style.width = leftRightWidth + 'px'
+  wallRight.style.right = -(leftRightWidth + wallThickness) + 'px'
+}
+
+window.addEventListener('resize', resizeGame)
+resizeGame()
+
 function Factory(el, name, place, angle = 0) {
   var div = document.createElement('div')
   div.setAttribute('class', name)
-  div.style.left = place[0] * 20 + 'px'
-  div.style.top = place[1] * 20 + 'px'
+  div.style.left = place[0] * cellSize + 'px'
+  div.style.top = place[1] * cellSize + 'px'
   if (angle) {
     div.style.transform = "rotate(" + angle + "deg)"
   }
@@ -91,13 +145,13 @@ function Food() {
   var food = new Factory(foodWrap, 'food', this.place)
   this.create = function () {
     var place = []
-    place[0] = Math.ceil(Math.random() * 35)
-    place[1] = Math.ceil(Math.random() * 35)
+    place[0] = Math.floor(Math.random() * maxCols)
+    place[1] = Math.floor(Math.random() * maxRows)
     game.snake.info.forEach(v => {
       if (v[0] == place[0] && v[1] == place[1]) {
         console.log('食物在蛇身上', place, game.snake.info)
-        place[0] = Math.ceil(Math.random() * 35)
-        place[1] = Math.ceil(Math.random() * 35)
+        place[0] = Math.floor(Math.random() * maxCols)
+        place[1] = Math.floor(Math.random() * maxRows)
       }
     })
     this.remove()
@@ -118,7 +172,7 @@ function Game() {
     this.food = new Food()
     this.timer = setInterval(() => {
       this.snake.move()
-      if (this.snake.snakeHead.place[0] < 0 || this.snake.snakeHead.place[0] > 35 || this.snake.snakeHead.place[1] < 0 || this.snake.snakeHead.place[1] > 35) {
+      if (this.snake.snakeHead.place[0] < 0 || this.snake.snakeHead.place[0] >= maxCols || this.snake.snakeHead.place[1] < 0 || this.snake.snakeHead.place[1] >= maxRows) {
         console.log('撞墙了')
         return this.snake.die(this.timer)
       } else if (this.snake.snakeHead.place[0] == this.food.place[0] && this.snake.snakeHead.place[1] == this.food.place[1]) {
@@ -149,7 +203,7 @@ function Game() {
   this.continue = function () {
     this.timer = setInterval(() => {
       this.snake.move()
-      if (this.snake.snakeHead.place[0] < 0 || this.snake.snakeHead.place[0] > 35 || this.snake.snakeHead.place[1] < 0 || this.snake.snakeHead.place[1] > 35) {
+      if (this.snake.snakeHead.place[0] < 0 || this.snake.snakeHead.place[0] >= maxCols || this.snake.snakeHead.place[1] < 0 || this.snake.snakeHead.place[1] >= maxRows) {
         console.log('撞墙了')
         return this.snake.die(this.timer)
       } else if (this.snake.snakeHead.place[0] == this.food.place[0] && this.snake.snakeHead.place[1] == this.food.place[1]) {
@@ -164,13 +218,19 @@ function Game() {
     snakeDom.innerHTML = null
     foodWrap.innerHTML = null
     startMask.style.display = 'block'
+    this.snake = null
+    this.food = null
   }
 }
 var game = new Game()
 start.addEventListener('click', function () {
   startMask.style.display = 'none'
   game.init()
-  audio.play()
+  try {
+    audio.play()
+  } catch (e) {
+    console.log('Audio play failed:', e)
+  }
 })
 core.addEventListener('click', function () {
   controlMask.style.display = 'block'
@@ -180,6 +240,113 @@ core.addEventListener('click', function () {
 control.addEventListener('click', function () {
   controlMask.style.display = 'none'
   game.continue()
-  audio.play()
+  try {
+    audio.play()
+  } catch (e) {
+    console.log('Audio play failed:', e)
+  }
+})
+
+function setDirection(dir) {
+  if (!game.snake || !game.snake.snakeHead) return
+  var cur = game.snake.snakeHead.direction
+  if (dir == 'left' && cur != 'right') {
+    game.snake.snakeHead.direction = 'left'
+  } else if (dir == 'top' && cur != 'bottom') {
+    game.snake.snakeHead.direction = 'top'
+  } else if (dir == 'right' && cur != 'left') {
+    game.snake.snakeHead.direction = 'right'
+  } else if (dir == 'bottom' && cur != 'top') {
+    game.snake.snakeHead.direction = 'bottom'
+  }
+}
+
+var directionBtns = document.querySelectorAll('.direction-btn')
+var holdTimer = null
+directionBtns.forEach(function (btn) {
+  function startHold(e) {
+    if (e) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+    var dir = this.getAttribute('data-dir')
+    setDirection(dir)
+    if (holdTimer) clearInterval(holdTimer)
+    holdTimer = setInterval(function () {
+      setDirection(dir)
+    }, 50)
+  }
+  function endHold(e) {
+    if (e) {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+    if (holdTimer) {
+      clearInterval(holdTimer)
+      holdTimer = null
+    }
+  }
+  btn.addEventListener('touchstart', startHold, { passive: false })
+  btn.addEventListener('touchend', endHold, { passive: false })
+  btn.addEventListener('touchmove', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+  }, { passive: false })
+  btn.addEventListener('touchcancel', endHold, { passive: false })
+  btn.addEventListener('mousedown', startHold)
+  btn.addEventListener('mouseup', endHold)
+  btn.addEventListener('mouseleave', endHold)
+  btn.addEventListener('click', function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+  })
+})
+
+var touchStartX = 0
+var touchStartY = 0
+var minSwipeDistance = 30
+var swipeBlocked = false
+
+document.addEventListener('touchstart', function (e) {
+  if (e.target.closest && e.target.closest('.direction-pad')) {
+    swipeBlocked = true
+    return
+  }
+  swipeBlocked = false
+  touchStartX = e.touches[0].clientX
+  touchStartY = e.touches[0].clientY
+}, { passive: false })
+
+document.addEventListener('touchmove', function (e) {
+  if (e.target.closest && e.target.closest('.direction-pad')) {
+    e.preventDefault()
+  }
+}, { passive: false })
+
+document.addEventListener('touchend', function (e) {
+  if (swipeBlocked) return
+  if (!game.snake || !game.snake.snakeHead) return
+  var touchEndX = e.changedTouches[0].clientX
+  var touchEndY = e.changedTouches[0].clientY
+  var deltaX = touchEndX - touchStartX
+  var deltaY = touchEndY - touchStartY
+
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    if (Math.abs(deltaX) > minSwipeDistance) {
+      if (deltaX > 0 && game.snake.snakeHead.direction != 'left') {
+        game.snake.snakeHead.direction = 'right'
+      } else if (deltaX < 0 && game.snake.snakeHead.direction != 'right') {
+        game.snake.snakeHead.direction = 'left'
+      }
+    }
+  } else {
+    if (Math.abs(deltaY) > minSwipeDistance) {
+      if (deltaY > 0 && game.snake.snakeHead.direction != 'top') {
+        game.snake.snakeHead.direction = 'bottom'
+      } else if (deltaY < 0 && game.snake.snakeHead.direction != 'bottom') {
+        game.snake.snakeHead.direction = 'top'
+      }
+    }
+  }
 })
 
